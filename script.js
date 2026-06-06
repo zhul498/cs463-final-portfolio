fbeb26fa0d3a832a6097ad3ce5b07dda368f8857
Lian <108500729+zhul498@users.noreply.github.com>
2026-06-05 22:37:06 -0700

    add contact form and javascript

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("test");
+const contactForm = document.getElementById("contact-form");
+const responseMessage = document.getElementById("form-response");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  responseMessage.innerHTML = `
+    <div class="submitted-message">
+      <h3>Message Submitted</h3>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    </div>
+  `;
+
+  contactForm.reset();
+});
